2ecb0352e2f59fecd7f02ac8c7fdb76a383d2cae
Shane Dorsey <scdorsey22@gmail.com>
2022-05-23 14:12:47 -0700

    commit tree 50fce06a884834f32c73437f824a89c2556f2358

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById('main')
+
+mainElement.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Shane is the champion"
